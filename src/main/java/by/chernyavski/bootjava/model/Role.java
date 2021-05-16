0bb8663232a9ac65b5086670313896bdd578aaf5
VladChernyavski <chernyavskivl@gmail.com>
2021-05-16 16:46:28 +0300

    add user with lombok

---
+++ src/main/java/by/chernyavski/bootjava/model/Role.java
@@ -0,0 +1,6 @@
+package by.chernyavski.bootjava.model;
+
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/by/chernyavski/bootjava/model/User.java
@@ -0,0 +1,24 @@
+package by.chernyavski.bootjava.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String password;
+
+    private Set<Role> roles;
+
+}
