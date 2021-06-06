acd1a7f03de828143e772f9d0095814ad557e041
VladChernyavski <chernyavskivl@gmail.com>
2021-06-06 15:54:20 +0300

    h2 init

--- src/main/java/by/chernyavski/bootjava/Application.java
+++ src/main/java/by/chernyavski/bootjava/Application.java
@@ -1,15 +1,11 @@
 package by.chernyavski.bootjava;
 
-import by.chernyavski.bootjava.model.Role;
-import by.chernyavski.bootjava.model.User;
 import by.chernyavski.bootjava.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Set;
 
 @SpringBootApplication
 @AllArgsConstructor
@@ -23,8 +19,6 @@ public class Application implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) {
-        userRepository.save(new User("vl@gmail.com", "Vlad", "Chernyavski", "12345", Set.of(Role.ROLE_ADMIN)));
-        userRepository.save(new User("user@gmail.com", "User", "User", "12345", Set.of(Role.ROLE_USER)));
         System.out.println(userRepository.findAll());
     }
 
